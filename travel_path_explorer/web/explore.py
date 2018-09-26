0ae430aadac1ad4f472738a2c10621cac0c983cf
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-09-26 11:25:26 +0800

    deal not found error in explore API

--- travel_path_explorer/web/explore.py
+++ travel_path_explorer/web/explore.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from googlemaps.exceptions import ApiError
 from ..explore import find_route
 from ..helper import convert_route_images_to_base64
 
@@ -10,6 +11,17 @@ def explore():
     origin = request.args['origin']
     destination = request.args['destination']
 
-    route = find_route(origin, destination)
-    convert_route_images_to_base64(route)
-    return jsonify(route)
+    result = {
+        "route": None,
+        "status": "Unknown"
+    }
+
+    try:
+        route = find_route(origin, destination)
+        convert_route_images_to_base64(route)
+        result["status"] = "OK"
+        result["route"] = route
+    except ApiError:
+        result["status"] = "NOT_FOUND"
+
+    return jsonify(result)
