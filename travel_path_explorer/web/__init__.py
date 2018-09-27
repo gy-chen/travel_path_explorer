505efbe3e8a8ffafe8d0b8e39eca577a6bdebb06
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-09-27 11:01:17 +0800

    add cors

--- travel_path_explorer/web/__init__.py
+++ travel_path_explorer/web/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from .explore import explore_bp
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     app.register_blueprint(explore_bp)
 
