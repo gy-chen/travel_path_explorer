9781b7419f1cfc5487426c8b4edea975dd15833d
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-09-30 16:16:32 +0800

    add flask-sqlalchemy

--- travel_path_explorer/web/__init__.py
+++ travel_path_explorer/web/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from .model import db
 from .explore import explore_bp
 
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+    db.init_app(app)
 
     app.register_blueprint(explore_bp)
 
