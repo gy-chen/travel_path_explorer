68fb4bb6bda7b259a173ec36c1f37951e8f0616f
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-10-02 10:35:03 +0800

    change geolocation model

--- travel_path_explorer/web/model/geolocation.py
+++ travel_path_explorer/web/model/geolocation.py
@@ -3,7 +3,12 @@ from . import db
 
 class GeoLocation(db.Model):
     __tablename__ = 'geolocation'
+    __table_args__ = (
+        db.PrimaryKeyConstraint('network_from', 'network_to'),
+    )
 
-    network = db.Column(db.String, primary_key=True)
     lat = db.Column(db.Float, nullable=False)
     lng = db.Column(db.Float, nullable=False)
+    network = db.Column(db.String, nullable=False)
+    network_from = db.Column(db.Integer)
+    network_to = db.Column(db.Integer)
