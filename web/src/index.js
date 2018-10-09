0d965f53cc2829eef1c265e2f372718e65715994
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-10-09 20:25:12 +0800

    fix prefixes of dotenv variables

--- web/src/index.js
+++ web/src/index.js
@@ -4,11 +4,17 @@ import ReactDOM from 'react-dom';
 import store from './store';
 import './index.css';
 import App from './App';
+import GoogleMapKeyContext from './component/GoogleMapKeyContext';
+import GoogleStaticMapKeyContext from './component/GoogleStaticMapKeyContext';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
     <Provider store={store}>
-        <App />
+        <GoogleStaticMapKeyContext.Provider value={process.env.REACT_APP_GMAPS_API_KEY}>
+            <GoogleMapKeyContext.Provider value={process.env.REACT_APP_GMAPS_API_KEY}>
+                <App />
+            </GoogleMapKeyContext.Provider>
+        </GoogleStaticMapKeyContext.Provider>
     </Provider>
 ), document.getElementById('root'));
 registerServiceWorker();
