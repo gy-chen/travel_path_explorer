a494229ff3c99f7a25529ff7ad0cd0a7b6a7a250
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-11-22 11:57:34 +0800

    remove unnecessary style

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import RedirectToCurrentLocale from './component/RedirectToCurrentLocale';
 import AppRoutes from './AppRoutes';
 import { currentGeolocation } from './action';
 import MatomoTracker from './component/MatomoTracker';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
 
 /**
  * App
@@ -29,13 +23,13 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Wrapper>
+        <div>
           <Switch>
             <Route path="/:locale" component={AppRoutes} />
             <Route path="/" component={RedirectToCurrentLocale} />
           </Switch>
           <MatomoTracker host={process.env.REACT_APP_TRACKER_SERVER} />
-        </Wrapper>
+        </div>
       </Router>
     );
   }
