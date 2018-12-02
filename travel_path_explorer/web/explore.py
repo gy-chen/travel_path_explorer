da8843ae662e55cf46b5fa1beb6141e2147536c0
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-12-02 10:52:55 +0800

    make route's images stored in server, instead of embed in response

--- travel_path_explorer/web/explore.py
+++ travel_path_explorer/web/explore.py
@@ -1,13 +1,22 @@
-from flask import Blueprint, request, jsonify
-from ..explore import find_route
-from ..helper import convert_route_images_to_base64
-from ..exception import *
+from functools import partial
+from flask import Blueprint, request, jsonify, url_for
+from . import file_storage
+from .. import explore
+from ..exception import TransportError, ApiError, NotFoundError
 
 explore_bp = Blueprint("explore", __name__)
 
 
+def extract_image(content):
+    name = file_storage.file_storage.put(content)
+    return url_for('file_storage.get', name=name, _external=True)
+
+
+find_route = partial(explore.find_route, extract_image=extract_image)
+
+
 @explore_bp.route('/explore')
-def explore():
+def explore_():
     origin = request.args['origin']
     destination = request.args['destination']
 
@@ -18,7 +27,6 @@ def explore():
 
     try:
         route = find_route(origin, destination)
-        convert_route_images_to_base64(route)
         result["status"] = "OK"
         result["route"] = route
     except NotFoundError:
